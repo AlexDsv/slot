febccf70becb19c1d66e0aff48a7b08b68e48fd7
AlexDsv <Alex.dsv@hotmail.fr>
2023-12-20 17:17:48 +0100

    base & animation done/working on bet & wallet

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,28 @@ interface Symbol {
     imageUrl: string;
 }
 
-const symbols: Symbol[] = [{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"}, {name: "club", imageUrl: "../assets/club.png"}, {name: "bonus", imageUrl: "../assets/bonus.png"}]
+interface SymbolData {
+    name: string;
+    index: number; 
+    reelIndex: number
+}
+
+const symbols: Symbol[] = [{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"}, {name: "club", imageUrl: "../assets/club.png"},
+                           {name: "bonus", imageUrl: "../assets/bonus.png"},{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"},
+                           {name: "club", imageUrl: "../assets/club.png"},{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"},
+                           {name: "club", imageUrl: "../assets/club.png"},{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"},
+                           {name: "club", imageUrl: "../assets/club.png"},{name: "spade", imageUrl: "../assets/spade.png"},{name: "heart", imageUrl: "../assets/heart.png"},
+                           {name: "club", imageUrl: "../assets/club.png"}]
+
+
 const reel1 = document.getElementById('reel1') as HTMLElement
 const reel2 = document.getElementById('reel2') as HTMLElement
 const reel3 = document.getElementById('reel3') as HTMLElement
 
-const generatedSymbolsReel1: string[] = [];
-const generatedSymbolsReel2: string[] = [];
-const generatedSymbolsReel3: string[] = [];
+
+const generatedSymbolsReel1: SymbolData[] = [];
+const generatedSymbolsReel2: SymbolData[] = [];
+const generatedSymbolsReel3: SymbolData[] = [];
 const shuffleArray = (array: Symbol[]): Symbol[] => {
     for(let i = array.length -1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -22,63 +36,44 @@ const shuffleArray = (array: Symbol[]): Symbol[] => {
     return array
 }
 
-function addSymbolsToReel(symbols: Symbol[], reelContainer: HTMLElement, generatedSymbols: string[]): void{
+function addSymbolsToReel(symbols: Symbol[], reelContainer: HTMLElement, generatedSymbols: SymbolData[], reelIndex: number): void{
 
-    symbols.forEach(symbol => {
+    symbols.forEach((symbol, index) => {
         const symbolDiv = document.createElement('div');
         symbolDiv.classList.add('symbol');
+        symbolDiv.setAttribute('data-index', `${index}`)
+        symbolDiv.setAttribute('data-reel', `${reelIndex}`)
         const symbolImage = document.createElement('img');
         symbolImage.src = symbol.imageUrl;
         symbolImage.alt = symbol.name;
 
         symbolDiv.appendChild(symbolImage);
         reelContainer.appendChild(symbolDiv)
-        generatedSymbols.push(symbol.name)
+        generatedSymbols.push({name: symbol.name, index: index, reelIndex: reelIndex})
     })
 
 }
 
-addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
+addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1, 1)
+addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2, 2)
+addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3, 3)
 
 
-function applyBounceToWinningSymbols(lineIndex: number) {
 
-    const reel1Symbol = document.querySelector(`#reel1 .symbol:nth-child(${lineIndex + 1})`);
-    const reel2Symbol = document.querySelector(`#reel2 .symbol:nth-child(${lineIndex + 1})`);
-    const reel3Symbol = document.querySelector(`#reel3 .symbol:nth-child(${lineIndex + 1})`);
+function applyBounceToWinningSymbols(visibleSymbolsReel1: SymbolData[], visibleSymbolsReel2: SymbolData[], visibleSymbolsReel3: SymbolData[], lineIndex: number) {
+    const winningSymbol1 = visibleSymbolsReel1[lineIndex];
+    const winningSymbol2 = visibleSymbolsReel2[lineIndex];
+    const winningSymbol3 = visibleSymbolsReel3[lineIndex];
+
+    const reel1Symbol = document.querySelector(`#reel${winningSymbol1.reelIndex} .symbol[data-index="${winningSymbol1.index}"]`);
+    const reel2Symbol = document.querySelector(`#reel${winningSymbol2.reelIndex} .symbol[data-index="${winningSymbol2.index}"]`);
+    const reel3Symbol = document.querySelector(`#reel${winningSymbol3.reelIndex} .symbol[data-index="${winningSymbol3.index}"]`);
 
     reel1Symbol?.classList.add("bounce");
     reel2Symbol?.classList.add("bounce");
     reel3Symbol?.classList.add("bounce");
 }
 
-function applyBounceToDiagonalWinners(isDiagonalWinner: boolean, diagonal: string) {
-    if (isDiagonalWinner) {
-        if (diagonal === 'topLeftToBottomRight') {
-            document.querySelector('#reel1 .symbol:nth-child(1)')?.classList.add('bounce');
-            document.querySelector('#reel2 .symbol:nth-child(2)')?.classList.add('bounce');
-            document.querySelector('#reel3 .symbol:nth-child(3)')?.classList.add('bounce');
-        } else if (diagonal === 'bottomLeftToTopRight') {
-            document.querySelector('#reel1 .symbol:nth-child(3)')?.classList.add('bounce');
-            document.querySelector('#reel2 .symbol:nth-child(2)')?.classList.add('bounce');
-            document.querySelector('#reel3 .symbol:nth-child(1)')?.classList.add('bounce');
-        }
-    }
-}
 
 
 function checkForWinningLinesAndApplyBounce() {
@@ -93,25 +88,33 @@ function checkForWinningLinesAndApplyBounce() {
     const visibleSymbolsReel2 = generatedSymbolsReel2.slice(firstVisibleSymbolIndex, firstVisibleSymbolIndex + numberOfVisibleSymbols);
     const visibleSymbolsReel3 = generatedSymbolsReel3.slice(firstVisibleSymbolIndex, firstVisibleSymbolIndex + numberOfVisibleSymbols);
     
-    if(visibleSymbolsReel1[0] === visibleSymbolsReel2[0] && visibleSymbolsReel2[0] === visibleSymbolsReel3[0])
+    
+    if(visibleSymbolsReel1[0].name === visibleSymbolsReel2[0].name && visibleSymbolsReel2[0].name === visibleSymbolsReel3[0].name)
     {
-        applyBounceToWinningSymbols(0)
-        console.log("Line 1 win");
+
+        applyBounceToWinningSymbols(visibleSymbolsReel1, visibleSymbolsReel2, visibleSymbolsReel3, 0);
     }
-    if(visibleSymbolsReel1[1] === visibleSymbolsReel2[1] && visibleSymbolsReel2[1] === visibleSymbolsReel3[1])
+    if(visibleSymbolsReel1[1].name === visibleSymbolsReel2[1].name && visibleSymbolsReel2[1].name === visibleSymbolsReel3[1].name)
     {
-        applyBounceToWinningSymbols(1)
-        console.log("Line 2 win");
+        applyBounceToWinningSymbols(visibleSymbolsReel1, visibleSymbolsReel2, visibleSymbolsReel3, 1)
     }
-    if(visibleSymbolsReel1[2] === visibleSymbolsReel2[2] && visibleSymbolsReel2[2] === visibleSymbolsReel3[2])
+    if(visibleSymbolsReel1[2].name === visibleSymbolsReel2[2].name && visibleSymbolsReel2[2].name === visibleSymbolsReel3[2].name)
     {
-        applyBounceToWinningSymbols(2)
-        console.log("Line 3 win");
+        applyBounceToWinningSymbols(visibleSymbolsReel1, visibleSymbolsReel2, visibleSymbolsReel3, 2)
     }
-    const isDiagonalWinner1 = visibleSymbolsReel1[0] === visibleSymbolsReel2[1] && visibleSymbolsReel2[1] === visibleSymbolsReel3[2];
-    applyBounceToDiagonalWinners(isDiagonalWinner1, 'topLeftToBottomRight');
-    const isDiagonalWinner2 = visibleSymbolsReel1[2] === visibleSymbolsReel2[1] && visibleSymbolsReel2[1] === visibleSymbolsReel3[0];
-    applyBounceToDiagonalWinners(isDiagonalWinner2, 'bottomLeftToTopRight');
+    if(visibleSymbolsReel1[0].name === visibleSymbolsReel2[1].name && visibleSymbolsReel2[1].name === visibleSymbolsReel3[2].name)
+    {
+        applyBounceToWinningSymbols(visibleSymbolsReel1, visibleSymbolsReel1, visibleSymbolsReel1, 0)
+        applyBounceToWinningSymbols(visibleSymbolsReel2, visibleSymbolsReel2, visibleSymbolsReel2, 1)
+        applyBounceToWinningSymbols(visibleSymbolsReel3, visibleSymbolsReel3, visibleSymbolsReel3, 2)
+    }
+    if(visibleSymbolsReel1[2].name === visibleSymbolsReel2[1].name && visibleSymbolsReel2[1].name === visibleSymbolsReel3[0].name)
+    {
+        applyBounceToWinningSymbols(visibleSymbolsReel3, visibleSymbolsReel3, visibleSymbolsReel3, 0)
+        applyBounceToWinningSymbols(visibleSymbolsReel2, visibleSymbolsReel2, visibleSymbolsReel2, 1)
+        applyBounceToWinningSymbols(visibleSymbolsReel1, visibleSymbolsReel1, visibleSymbolsReel1, 2)
+    }
+    
 }
 
 function resetAnimations() {
@@ -122,15 +125,22 @@ function startSpinning(reelSymbols: HTMLElement, animationDuration: number, call
     reelSymbols.style.animation = "none";
 
     setTimeout(() => {
-        reelSymbols.style.animation = `spin ${animationDuration}s linear forwards`;
+        reelSymbols.style.animation = `spin ${animationDuration}s ease-out forwards`;
         setTimeout(callback, animationDuration * 1000);
     }, 0);
 }
 
+let wallet: number = 10000;
+let bet: number = 10
 
 document.getElementById('spinButton')?.addEventListener('click', () => {
-    resetAnimations();
-
+    resetAnimations(); 
+    wallet = wallet - bet;
+    if (bet > wallet) {
+        console.log("Solde insuffisant pour ce pari");
+        return;
+    }
+   
     generatedSymbolsReel1.length = 0;
     generatedSymbolsReel2.length = 0;
     generatedSymbolsReel3.length = 0;
@@ -140,24 +150,10 @@ document.getElementById('spinButton')?.addEventListener('click', () => {
     reel1.innerHTML = ""
     reel2.innerHTML = ""
     reel3.innerHTML = ""
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
-    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1)
-    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2)
-    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3)
+    addSymbolsToReel(shuffleArray(symbols), reel1, generatedSymbolsReel1, 1)
+    addSymbolsToReel(shuffleArray(symbols), reel2, generatedSymbolsReel2, 2)
+    addSymbolsToReel(shuffleArray(symbols), reel3, generatedSymbolsReel3, 3)
+    
 
     
     startSpinning(reel1, animationDuration, checkForWinningLinesAndApplyBounce);
@@ -165,4 +161,71 @@ document.getElementById('spinButton')?.addEventListener('click', () => {
     startSpinning(reel3, animationDuration, checkForWinningLinesAndApplyBounce)
 
     checkForWinningLinesAndApplyBounce();
+    
+    updateBetDisplay(bet)
+})
+
+
+
+const betBtn10 = document.getElementById('betButton10')
+const betBtn50 = document.getElementById('betButton50')
+const betBtn100 = document.getElementById('betButton100')
+const betBtn250 = document.getElementById('betButton250')
+const betBtn500 = document.getElementById('betButton500')
+const betBtn750 = document.getElementById('betButton750')
+const betBtn1000 = document.getElementById('betButton1000')
+
+function updateBetDisplay(bet: number) {
+    const betElement = document.getElementById('bet');
+    const walletElement = document.getElementById('wallet');
+    const spinButton = document.getElementById('spinButton') as HTMLButtonElement;
+
+    if (walletElement) {
+        walletElement.textContent = `Solde: ${wallet}`
+    }
+    if (betElement) {
+        betElement.textContent = `Bet: ${bet} €`;
+    }
+
+    if (spinButton) {
+        spinButton.disabled = bet > wallet;
+    }
+}
+
+
+updateBetDisplay(bet);
+
+betBtn10?.addEventListener('click', () => {
+    bet = 10
+    updateBetDisplay(bet);
+})
+
+betBtn50?.addEventListener('click', () => {
+    bet = 50
+    updateBetDisplay(bet);
+})
+
+betBtn100?.addEventListener('click', () => {
+    bet = 100
+    updateBetDisplay(bet);
+})
+
+betBtn250?.addEventListener('click', () => {
+    bet = 250
+    updateBetDisplay(bet);
+})
+
+betBtn500?.addEventListener('click', () => {
+    bet = 500
+    updateBetDisplay(bet);
+})
+
+betBtn750?.addEventListener('click', () => {
+    bet = 750
+    updateBetDisplay(bet);
+})
+
+betBtn1000?.addEventListener('click', () => {
+    bet = 1000
+    updateBetDisplay(bet);
 })
